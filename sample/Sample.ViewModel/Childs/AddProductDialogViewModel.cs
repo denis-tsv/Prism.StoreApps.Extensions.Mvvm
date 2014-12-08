a40346573b9d9326a43c8cb14d88c2cc74dfe0e6
denis-tsv <yadeniskotcvet@gmail.com>
2014-12-08 14:37:00 +0700

    Attach dialog view model

---
+++ sample/Sample.ViewModel/Childs/AddProductDialogViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Prism.StoreApps.Extensions.ViewModel.ViewModels.Childs;
+using ReactiveUI;
+using Sample.ViewModel.Regions;
+
+namespace Sample.ViewModel.Childs
+{
+    public class AddProductDialogViewModel : DialogChildViewModel
+    {
+        [Dependency]
+        public DetailsRegionViewModel DetailsRegionViewModel { get; set; }
+
+        protected override IObservable<bool> GetOkCanExecute()
+        {
+            return DetailsRegionViewModel.Product.WhenAnyValue(product => product.Name, (name) => !string.IsNullOrEmpty(name));
+        }
+    }
+}
